a089834bd36e74c7308f1581f20c74e60fe9a4ab
mayuresh-tech9 <mayuresh.gramopadhye@tech9.com>
2024-09-19 19:38:27 +0530

    Updated data

--- data/resume.tsx
+++ data/resume.tsx
@@ -3,13 +3,13 @@ import { Icons } from "@/components/icons";
 export const DATA = {
   name: "Mayuresh Gramopadhye",
   initials: "MG",
-  url: "https://dillion.io",
+  url: "https://portfolio-eight-nu-96.vercel.app/",
   location: "India",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
-      "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+      "Software Engineer | PHP | Laravel | ReactJS | NextJS | ElectronJS | SQL | AWS at Tech9",
   summary:
-      "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+      "Software Engineer | PHP | Laravel | ReactJS | NextJS | ElectronJS | SQL | AWS at Tech9",
   avatarUrl: "/me.png",
   contact: {
     email: "mayuresh3@gmail.com",
