de88517a689777d140f2153531ef368de46cf4e4
mayuresh-tech9 <mayuresh.gramopadhye@tech9.com>
2024-09-19 19:28:17 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import Navbar from "@/components/navbar";
-import { ThemeProvider } from "@/components/theme-provider";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { DATA } from "@/data/resume";
-import { cn } from "@/lib/utils";
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import {ThemeProvider} from "@/components/theme-provider";
+import {TooltipProvider} from "@/components/ui/tooltip";
+import {DATA} from "@/data/resume";
+import type {Metadata} from "next";
 import "./globals.css";
-
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export const metadata: Metadata = {
   metadataBase: new URL(DATA.url),
